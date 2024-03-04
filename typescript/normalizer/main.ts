a4fe95ca8095a8f5129d9ed9e8dbf35122083c0d
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-04 02:40:47 +0200

    *

--- typescript/normalizer/main.ts
+++ typescript/normalizer/main.ts
@@ -1,6 +1,6 @@
 import { tokenizer } from "./tokenizer"
 
-const lambdaCalculusLarge = `(λx:Int.λy:Float.x) (λz:Int -> Int -> Nat.z) (λw:Nil.w)`
+const lambdaCalculusLarge = `(λx:Int.λy:Float.x) c (λw:Nil.w)`
 function main(){
   const lex = tokenizer(lambdaCalculusLarge);
   console.log(lex.getTokens());
